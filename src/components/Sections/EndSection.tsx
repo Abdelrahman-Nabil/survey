adaac936c23a7ac29f70153d89cee79247c34024
Abdelrahman Nabil <abdelrahman.nabil96@gmail.com>
2023-01-14 13:01:44 +0200

    few tweaks

--- src/components/Sections/EndSection.tsx
+++ src/components/Sections/EndSection.tsx
@@ -8,7 +8,7 @@ const EndSection = (props: ISectionProps) => {
     return (
         <Box>
             <Typography variant="h4">{t('surveyEnded')}</Typography>
-            <Typography sx={{ mt: 4 }} variant="h5">{props.endText}</Typography>
+            <Typography sx={{ mt: 4, maxWidth: 600, minWidth: 200 }} variant="h5">{props.endText}</Typography>
             <Button sx={{ height: 40, marginTop: 8 }} onClick={props.onConfirm} variant="contained">
                 {t('done')}
             </Button>
